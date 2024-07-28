80f91cb7066066e31c8fe975f7803097c8746abc
bluocat <124224350+bluocat@users.noreply.github.com>
2024-07-28 15:49:50 -0700

    Create function that plays one rps round

--- script.js
+++ script.js
@@ -22,3 +22,24 @@ function getHumanChoice() {
             return "Invalid response";
     }
 }
+
+let humanScore = 0,
+    computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "Invalid response") {
+        console.log("Invalid player response");
+    } else if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else if (humanChoice === "rock" && computerChoice === "scissors"
+        || humanChoice === "scissors" && computerChoice === "paper"
+        || humanChoice === "paper" && computerChoice === "rock"
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+        humanScore++
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+        computerScore++
+    }
+}
+
